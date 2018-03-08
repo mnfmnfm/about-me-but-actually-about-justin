c8cf32e05d69e135f36637560373cb8d25b4892e
Katy Bowers <katyvine@gmail.com>
2018-03-08 15:25:29 -0800

    Updated JS file with pair programming to incude conditions in functions

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var questAboutMe = [ // Creating an array that will store my questions for the q
 
   'Do you think I was once hit in the head with a chunk of asphalt?',
 
-  'Would you believe me if I sgiaid I met my wife on "The Bachelorette"?',
+  'Would you believe me if I said I met my wife on "The Bachelorette"?',
 
   'Would you believe that I was once struck by lightning?',
 
@@ -21,8 +21,15 @@ var correctAnsw = ['n', 'y', 'y', 'n', 'n']; // Creating an array that determine
 
 var incorrectAnsw = ['y', 'n', 'n', 'y', 'y']; // Creating an array allowing me to display the incorrect responses.
 
-var numGuitars = Math.floor((Math.random() * 20) + 1); //Creating a random answer for # of guitars.
-console.log('This how many guitars: ' + numGuitars);
+var numGuitars = 0;
+
+function randNumGuitars() {
+  numGuitars = Math.floor((Math.random() * 20) + 1); //Creating a random answer for # of guitars.
+  console.log('This how many guitars: ' + numGuitars);
+  return numGuitars;
+}
+
+randNumGuitars();
 
 var corrResp = [ // Another array that is storing the responses that will be given when the user answers correctly.
   'I can\'t fool you. I was actually born in the small town of Deer Park, Washington.',
@@ -58,9 +65,13 @@ var statesVisited = ['washington', 'hawaii', 'idaho', 'montana', 'california', '
 
 var statesVisitedFormat = []; // Will be used to display states for question #7. Comes in handy later.
 
-for (var k = 0; k < statesVisited.length; k++) { // Created a 'space' and capital letter for the state.
-  statesVisitedFormat[k] = ' ' + statesVisited[k].substring(0, 1).toUpperCase() + statesVisited[k].slice(1);
+function formatStatesFunction(){
+  for (var k = 0; k < statesVisited.length; k++) { // Created a 'space' and capital letter for the state.
+    statesVisitedFormat[k] = ' ' + statesVisited[k].substring(0, 1).toUpperCase() + statesVisited[k].slice(1);
+  }
+  return statesVisitedFormat;
 }
+formatStatesFunction();
 
 var userResp; // A variable that will be used to gather the user's response.
 
@@ -72,97 +83,103 @@ var isCorrect; // This will be a boolean value to see determine if they got ques
 
 alert('You\'re ready to take the quiz! Please answer the following questions with "yes" or "no".'); // An alert that begins the quiz and tells the user to enter "yes" or "no".
 
-for (var i = 0; i < questAboutMe.length; i++) {
+function launchQuiz(){
 
-  if (i === 5) { // Adjust questioning for Question #6
+  for (var i = 0; i < questAboutMe.length; i++) {
 
-    alert('Time for some bonus questions!! Can you guess the right answer?');
+    if (i === 5) { // Adjust questioning for Question #6
 
-    var numOfGuesses = 5;
+      alert('Time for some bonus questions!! Can you guess the right answer?');
 
-    while (numOfGuesses > 0) {
-      var userNumResp = prompt(questAboutMe[i] + ' (It\'s between 1 and 20.) You have ' + numOfGuesses + ' out of 5 guesses left.');
-      console.log('This is the user\'s input: ' + userNumResp);
-      userNumResp = parseInt(userNumResp, 10); // Now user input is an integer
-      console.log('This is the user\'s input as an integer: ' + userNumResp);
+      var numOfGuesses = 5;
 
-      if (userNumResp === numGuitars){
-        alert('Nice guess! You are correct!');
-        score++;
-        isCorrect = true;
-        console.log('The user\'s score so far: Score = ' + score);
-        break;
-      } else {
-        numOfGuesses--;
-      }
-    }
+      while (numOfGuesses > 0) {
+        var userNumResp = prompt(questAboutMe[i] + ' (It\'s between 1 and 20.) You have ' + numOfGuesses + ' out of 5 guesses left.');
+        console.log('This is the user\'s input: ' + userNumResp);
+        userNumResp = parseInt(userNumResp, 10); // Now user input is an integer
+        console.log('This is the user\'s input as an integer: ' + userNumResp);
 
-    if (!isCorrect){ //If we go through all of our guesses this displays on alert before moving to next question.
-      alert('Nice try but I have ' + numGuitars + '.');
-    }
-
-  } else if (i === 6) { // adjusting for question #7
-
-    alert('Here\'s another bonus question!');
-
-    numOfGuesses = 6;
-
-    while (numOfGuesses > 0) {
-      userResp = prompt(questAboutMe[i] + ' There\'s 7 of them. You have ' + numOfGuesses + ' out of 6 guesses left.');
-      console.log('This is the user\'s input: ' + userNumResp);
-      shrtUserResp = userResp.toLowerCase();
-      console.log('This is the user\'s response lowercased: ' + shrtUserResp);
-
-      for (var j = 0; j < statesVisited.length; j++){
-        if (shrtUserResp === statesVisited[j]) {
+        if (userNumResp === numGuitars){
+          alert('Nice guess! You are correct!');
+          score++;
           isCorrect = true;
+          console.log('The user\'s score so far: Score = ' + score);
           break;
         } else {
-          isCorrect = false;
+          numOfGuesses--;
         }
       }
 
-      if (isCorrect){
-        alert('Nice guess! You are correct! Here were all of the possible answers:' + statesVisitedFormat.toString() + '.' );
-        score++;
-        console.log('The user\'s score so far: Score = ' + score);
-        break;
-      } else {
-        numOfGuesses--;
+      if (!isCorrect){ //If we go through all of our guesses this displays on alert before moving to next question.
+        alert('Nice try but I have ' + numGuitars + '.');
       }
+
+    } else if (i === 6) { // adjusting for question #7
+
+      alert('Here\'s another bonus question!');
+
+      numOfGuesses = 6;
+
+      while (numOfGuesses > 0) {
+        userResp = prompt(questAboutMe[i] + ' There\'s 7 of them. You have ' + numOfGuesses + ' out of 6 guesses left.');
+        console.log('This is the user\'s input: ' + userNumResp);
+        shrtUserResp = userResp.toLowerCase();
+        console.log('This is the user\'s response lowercased: ' + shrtUserResp);
+
+        for (var j = 0; j < statesVisited.length; j++){
+          if (shrtUserResp === statesVisited[j]) {
+            isCorrect = true;
+            break;
+          } else {
+            isCorrect = false;
+          }
+        }
+
+        if (isCorrect){
+          alert('Nice guess! You are correct! Here were all of the possible answers:' + statesVisitedFormat.toString() + '.' );
+          score++;
+          console.log('The user\'s score so far: Score = ' + score);
+          break;
+        } else {
+          numOfGuesses--;
+        }
+      }
+
+      if (!isCorrect){
+        alert('At least you tried! Here were all the possible answers you could have put:' + statesVisitedFormat.toString() + '.');
+      }
+
     }
+    else {
 
-    if (!isCorrect){
-      alert('At least you tried! Here were all the possible answers you could have put:' + statesVisitedFormat.toString() + '.');
-    }
+      userResp = prompt(questAboutMe[i]);
+      console.log('This is the user\'s response: ' + userResp); //Displaying answer given in console.
 
-  }
-  else {
+      if (userResp) { // This first if else is used to handel the \'null\' or \'undefined\'. If the user enters something userResp is equal to that but if box is empty or they cancel userResp = 'canceled'.
 
-    userResp = prompt(questAboutMe[i]);
-    console.log('This is the user\'s response: ' + userResp); //Displaying answer given in console.
+        shrtUserResp = userResp.substring(0, 1).toLowerCase();
+        console.log('This is the user\'s shortened response:  ' + shrtUserResp); // Displaying shortened answer to be compared.
+      } else {
+        shrtUserResp = 'c'; // If the user cancels or doesn't type antyhing we set shrtUserResp to 'c' for 'canceled'.
+      }
 
-    if (userResp) { // This first if else is used to handel the \'null\' or \'undefined\'. If the user enters something userResp is equal to that but if box is empty or they cancel userResp = 'canceled'.
-
-      shrtUserResp = userResp.substring(0, 1).toLowerCase();
-      console.log('This is the user\'s shortened response:  ' + shrtUserResp); // Displaying shortened answer to be compared.
-    } else {
-      shrtUserResp = 'c'; // If the user cancels or doesn't type antyhing we set shrtUserResp to 'c' for 'canceled'.
-    }
-
-    if (shrtUserResp === correctAnsw[i]) { // This if else if statement is used to go through questions, gather response, and display correct or incorrect messages.
-      alert(corrResp[i]);
-      score++; // Adding correct response to the user's score
-      console.log('The user\'s score so far: Score = ' + score); // Displaying current score in the console
-    } else if (shrtUserResp === incorrectAnsw[i]) {
-      alert(incorrResp[i]); // Firing incorrect response if user is wrong.
-      console.log('The user\'s score so far: Score = ' + score); // Not adding to score but still displaying their current score even if they get it wrong.
-    } else {
-      alert('I\'m not sure what you put but you didn\'t say "yes" or "no".');
-      console.log('The user\'s score so far: Score = ' + score);
+      if (shrtUserResp === correctAnsw[i]) { // This if else if statement is used to go through questions, gather response, and display correct or incorrect messages.
+        alert(corrResp[i]);
+        score++; // Adding correct response to the user's score
+        console.log('The user\'s score so far: Score = ' + score); // Displaying current score in the console
+      } else if (shrtUserResp === incorrectAnsw[i]) {
+        alert(incorrResp[i]); // Firing incorrect response if user is wrong.
+        console.log('The user\'s score so far: Score = ' + score); // Not adding to score but still displaying their current score even if they get it wrong.
+      } else {
+        alert('I\'m not sure what you put but you didn\'t say "yes" or "no".');
+        console.log('The user\'s score so far: Score = ' + score);
+      }
     }
   }
 }
+
+launchQuiz();
+
 alert('That\'s it! Let\'s see what your score was.');
 document.getElementById('scoreHead').style.display = 'inherit'; // Displaying Score Header after quiz is taken.
 document.getElementById('score').innerHTML = score + '/5'; // Displaying Total Score after quiz is taken.
